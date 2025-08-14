0e809a7fe823ca585eb9378a26eb499fe3dccd60
reesick <shreevirajmatale25@gmail.com>
2025-08-14 17:19:48 +0530

    patterns done

--- 01_basics/patterns/22.cpp
+++ 01_basics/patterns/22.cpp
@@ -2,6 +2,35 @@
 using namespace std;
 
 int main(){
+    int n;  
+    n = 4;
+    for (int i = 0; i < 7; i++)
+    {   
+        for (int j = 0; j < 7; j++){
+            
+           if (i == 0 || i == 6||j == 0 || j == 6)
+         {
+             n = 4;
+        }
+        else if (i == 1 || i == 5 ||j == 1 || j == 5)
+        {
+            n = 3;
+        }
+        else if (i == 2 || i == 4||j == 2 || j == 4)
+        {
+            n = 2;
+        }
+        else if (i == 3 && j == 3 )
+        {
+            n = 1;
+        }
+        
+        
+            cout << n;
+        }
+        cout << endl;
+        n--;
+    }
     
     return 0;
 }
